d117c3e25c8b9f6149f2a64bb2c8c0490f3566eb
SimoneChiodo <simochi.005@gmail.com>
2025-11-13 18:08:41 +0100

    Add logic to markAsDone button in homepage

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('addTask/', views.addTask, name='addTask'),
+  path('markAsDone/<int:pk>/', views.markAsDone, name='markAsDone'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from .models import Task
 
 def addTask(request):
   task = request.POST['task']
   Task.objects.create(task=task) # Imposto il nome della task a quella passata nella POST (non inserisco anche is_completed perché è a False di default)
+  return redirect('home')
+
+def markAsDone(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.is_completed = True
+  task.save()
   return redirect('home')
